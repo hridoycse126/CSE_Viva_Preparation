baa7744f116f8ebc823f643f6912663ebf981f05
MrHridoy <hridoyking5464@gmail.com>
2020-08-13 23:25:58 +0600

    3rd

---
+++ odd&Even.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    if(n%2==0){
+        printf("%d is Even number\n",n);
+    }
+    else{
+        printf("%d is Odd number\n",n);
+    }
+
+
+}
